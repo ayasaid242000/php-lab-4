3cb0dacc97ec1bf8a1d8d2d6c94b44ca15efc504
aya <118894683+ayasaid242000@users.noreply.github.com>
2023-04-10 05:39:24 +0200

    Update index.php
    editing

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
 </head>
 <body>
 	<center>
-		<h1>User Registeration Form</h1>
+		<h1>User Registration Form</h1>
+		<p> please fill this form and submit to add user record to the database </p>
 		<form action="insert.php" method="post">
 			
 <p>
-			<label for="firstName"> First Name:</label>
+			<label for="firstName">First Name:</label>
 			<input type="text" name="first_name" id="firstName">
 			</p>
 
@@ -19,32 +20,36 @@
 			<input type="text" name="last_name" id="lastName">
 			</p>
 
- <p>
-			<label for="emailAddress">Email Address:</label>
-			<input type="email" name="email" id="emailAddress">
+			
+<p>
+			
 			</p>
 
-<!-- <p>
+			
+<p>
 			<label for="Address">Address:</label>
 			<input type="text" name="address" id="Address">
-			</p> -->
+			</p>
 
 			
-<p>          
+<p>
+			<label for="emailAddress">Email Address:</label>
+			<input type="text" name="email" id="emailAddress">
+			</p>
+
+			
             <label for="Gender">Gender:</label>
             <input type='radio' id="Gender" name='gender' value='male' >male 
             <input type='radio' id="Gender" name='gender' value='female'>female </br></br>
-
-			<!-- <label for="Gender">Gender:</label>
-			<input type="text" name="gender" id="Gender"> -->
-			</p>
-
-			
-
+<p>
+			<label  for="gridCheck1">Receive E-mail from us</label>
+			<input  type="checkbox" id="gridCheck1" name ='receive'>
+            </p>
 
 			<input type="submit" value="Submit">
-            <button type='reset'> cancel </button>
+			<button type='reset'> cancel </button>
 		</form>
 	</center>
 </body>
 </html>
+
